bad4a471414f2e3fb6eeba5e2f90ff30df7135c2
Zack Teater <zteater@gmail.com>
2021-02-09 18:02:12 -0500

    update javadoc for java 11, fixes #159

--- src/main/java/io/whelk/flesch/kincaid/ComprehendValidator.java
+++ src/main/java/io/whelk/flesch/kincaid/ComprehendValidator.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 Whelk Contributors (http://whelk.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.whelk.flesch.kincaid;
 
 import static com.amazonaws.services.comprehend.model.PartOfSpeechTagType.ADJ;
@@ -15,45 +30,49 @@ import static com.amazonaws.services.comprehend.model.PartOfSpeechTagType.PRON;
 import static com.amazonaws.services.comprehend.model.PartOfSpeechTagType.PROPN;
 import static com.amazonaws.services.comprehend.model.PartOfSpeechTagType.SCONJ;
 import static com.amazonaws.services.comprehend.model.PartOfSpeechTagType.VERB;
-import java.util.Arrays;
+
 import java.util.List;
+
 import com.amazonaws.services.comprehend.model.SyntaxToken;
+
 import lombok.experimental.UtilityClass;
 
+/**
+ * @author Zack Teater
+ * @since 0.1.3
+ */
 @UtilityClass
 public class ComprehendValidator {
-  
-  private static final List<String> validTagTypes =
-      Arrays.asList(
-          ADJ.name(),
-          ADP.name(),
-          ADV.name(),
-          AUX.name(),
-          CONJ.name(),
-          CCONJ.name(),
-          DET.name(),
-          INTJ.name(),
-          NOUN.name(),
-          NUM.name(),
-          PART.name(),
-          PRON.name(),
-          PROPN.name(),
-          SCONJ.name(),
-          VERB.name());
-  
+
+  private static final List<String> validTagTypes = List.of( //
+      ADJ.name(), //
+      ADP.name(), //
+      ADV.name(), //
+      AUX.name(), //
+      CONJ.name(), //
+      CCONJ.name(), //
+      DET.name(), //
+      INTJ.name(), //
+      NOUN.name(), //
+      NUM.name(), //
+      PART.name(), //
+      PRON.name(), //
+      PROPN.name(), //
+      SCONJ.name(), //
+      VERB.name());
+
   /**
-   * Determine if <code>syntaxToken</code> is valid word. Unknown tags, punctuation, or symbols are
-   * not considered valid words.
+   * Determine if <code>syntaxToken</code> is valid word. Unknown tags,
+   * punctuation, or symbols are not considered valid words.
    * 
-   * 
-   * @param syntaxToken
-   * @return
+   * @param syntaxToken to validate
+   * @return true if word, otherwise false
    */
-  public static boolean isWord(SyntaxToken syntaxToken) { 
-    return syntaxToken != null &&
-           syntaxToken.getPartOfSpeech() != null &&
-           syntaxToken.getPartOfSpeech().getTag() != null &&
-           validTagTypes.contains(syntaxToken.getPartOfSpeech().getTag());
+  public static boolean isWord(SyntaxToken syntaxToken) {
+    return syntaxToken != null && //
+        syntaxToken.getPartOfSpeech() != null && //
+        syntaxToken.getPartOfSpeech().getTag() != null && //
+        validTagTypes.contains(syntaxToken.getPartOfSpeech().getTag());
   }
 
 }
--- src/main/java/io/whelk/flesch/kincaid/ReadabilityCalculator.java
+++ src/main/java/io/whelk/flesch/kincaid/ReadabilityCalculator.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 Whelk Contributors (http://whelk.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.whelk.flesch.kincaid;
 
 import java.util.List;
@@ -6,32 +21,30 @@ import io.whelk.hy.phen.Hyphenator;
 import lombok.experimental.UtilityClass;
 
 /**
- * 
  * @author Zack Teater
- * @version 0.0.20
- *
+ * @since 0.1.3
  */
 @UtilityClass
 public class ReadabilityCalculator {
-  
+
   /**
    * {@code content} parsed to {@code List<Sentence>} and overloaded to
-   * {@link #calculateReadingEase(List) calculateReadingEase(List&lt;Sentence&gt;, List&lt;String&gt;)}
+   * {@code calculateReadingEase(List)}
    * 
    * @param content to lex
-   * @return
+   * @return reading ease level
    */
   public static double calculateReadingEase(String content) {
     var sentences = Tokenizer.tokenizeContent(content);
     var words = Tokenizer.tokenizeSentences(sentences);
-    
+
     return ReadabilityCalculator.calculateReadingEase(sentences, words);
   }
-  
+
   /**
-   * In the Flesch reading-ease test, higher scores indicate material that is easier to read; lower
-   * numbers mark passages that are more difficult to read. The formula for the Flesch reading-ease
-   * score (FRES) test is:
+   * In the Flesch reading-ease test, higher scores indicate material that is
+   * easier to read; lower numbers mark passages that are more difficult to read.
+   * The formula for the Flesch reading-ease score (FRES) test is:
    * 
    * <pre>
    * 206.835 - 1.015 * (totalWords / totalSentences) - 84.6 * (totalSyllables / totalWords)
@@ -39,124 +52,136 @@ public class ReadabilityCalculator {
    * 
    * Scores can be interpreted as shown in the table below.
    * 
-   * <br><br>
-   * <table summary="score and school level mapping">
-   *    <tr>
-   *        <td><b>Score</b></td>
-   *        <td><b>School Level</b></td>
-   *        <td><b>Notes</b></td>
-   *    </tr><tr>
-   *        <td>100.0-90.0</td>
-   *        <td>5th grade</td>
-   *        <td>Very easy to read. Easily understood by an average 11-year-old student.</td>
-   *    </tr><tr>
-   *        <td>90.0-80.0</td>
-   *        <td>6th grade</td>
-   *        <td>Easy to read. Conversational English for consumers.</td>
-   *    </tr><tr>
-   *        <td>80.0-70.0</td>
-   *        <td>7th grade</td>
-   *        <td>Fairly easy to read.</td>
-   *    </tr><tr>
-   *        <td>70.0-60.0</td>
-   *        <td>8th &amp; 9th grade</td>
-   *        <td>Plain English. Easily understood by 13- to 15-year-old students.</td>
-   *    </tr><tr>
-   *        <td>60.0-50.0</td>
-   *        <td>10th to 12th grade</td>
-   *        <td>Fairly difficult to read.</td>
-   *    </tr><tr>
-   *        <td>50.0-30.0</td>
-   *        <td>College</td>
-   *        <td>Difficult to read.</td>
-   *    </tr><tr>
-   *        <td>30.0-0.0</td>
-   *        <td>College Graduate</td>
-   *        <td>Very difficult to read. Best understood by university graduates.</td>
-   *    </tr>
+   * <br>
+   * <br>
+   * <table>
+   * <caption>Reading-East Scores</caption>
+   * <tr>
+   * <td><b>Score</b></td>
+   * <td><b>School Level</b></td>
+   * <td><b>Notes</b></td>
+   * </tr>
+   * <tr>
+   * <td>100.0-90.0</td>
+   * <td>5th grade</td>
+   * <td>Very easy to read. Easily understood by an average 11-year-old
+   * student.</td>
+   * </tr>
+   * <tr>
+   * <td>90.0-80.0</td>
+   * <td>6th grade</td>
+   * <td>Easy to read. Conversational English for consumers.</td>
+   * </tr>
+   * <tr>
+   * <td>80.0-70.0</td>
+   * <td>7th grade</td>
+   * <td>Fairly easy to read.</td>
+   * </tr>
+   * <tr>
+   * <td>70.0-60.0</td>
+   * <td>8th &amp; 9th grade</td>
+   * <td>Plain English. Easily understood by 13- to 15-year-old students.</td>
+   * </tr>
+   * <tr>
+   * <td>60.0-50.0</td>
+   * <td>10th to 12th grade</td>
+   * <td>Fairly difficult to read.</td>
+   * </tr>
+   * <tr>
+   * <td>50.0-30.0</td>
+   * <td>College</td>
+   * <td>Difficult to read.</td>
+   * </tr>
+   * <tr>
+   * <td>30.0-0.0</td>
+   * <td>College Graduate</td>
+   * <td>Very difficult to read. Best understood by university graduates.</td>
+   * </tr>
    * </table>
    * 
    * @param sentences to lex
-   * @return
+   * @param words     already aggregate from sentences
+   * @return reading ease level
    */
   public static double calculateReadingEase(List<Sentence> sentences, List<String> words) {
     double totalSentences = sentences.size();
     double totalWords = words.size();
     double totalSyllables = countSyllables(words);
-    
+
     return 206.835 - 1.015 * (totalWords / totalSentences) - 84.6 * (totalSyllables / totalWords);
   }
-  
+
   /**
    * {@code content} parsed to {@code List<Sentence>} and overloaded to
-   * {@link #calculateGradeLevel(List) calculateGradeLevel(List&lt;Sentence&gt;, List&lt;String&gt;)}
+   * {@code calculateGradeLevel(List)}
    * 
    * @param content to lex
-   * @return
+   * @return reading grade level
    */
   public static double calculateGradeLevel(String content) {
     var sentences = Tokenizer.tokenizeContent(content);
     var words = Tokenizer.tokenizeSentences(sentences);
-    
+
     return ReadabilityCalculator.calculateGradeLevel(sentences, words);
   }
-  
+
   /**
-   * These readability tests are used extensively in the field of education. The "Flesch–Kincaid
-   * Grade Level Formula" instead presents a score as a U.S. grade level, making it easier for
-   * teachers, parents, librarians, and others to judge the readability level of various books and
-   * texts. It can also mean the number of years of education generally required to understand this
-   * text, relevant when the formula results in a number greater than 10. The grade level is
-   * calculated with the following formula:
+   * These readability tests are used extensively in the field of education. The
+   * "Flesch–Kincaid Grade Level Formula" instead presents a score as a U.S. grade
+   * level, making it easier for teachers, parents, librarians, and others to
+   * judge the readability level of various books and texts. It can also mean the
+   * number of years of education generally required to understand this text,
+   * relevant when the formula results in a number greater than 10. The grade
+   * level is calculated with the following formula:
    * 
    * <pre>
    * 0.39 * (totalWords / totalSentences) + 11.8 * (totalSyllables / totalWords) - 15.59;
    * </pre>
    * 
-   * The result is a number that corresponds with a U.S. grade level. The sentence, "The Australian
-   * platypus is seemingly a hybrid of a mammal and reptilian creature" is an 11.3 as it has 24
-   * syllables and 13 words. The different weighting factors for words per sentence and syllables
-   * per word in each scoring system mean that the two schemes are not directly comparable and
-   * cannot be converted. The grade level formula emphasises sentence length over word length. By
-   * creating one-word strings with hundreds of random characters, grade levels may be attained that
-   * are hundreds of times larger than high school completion in the United States. Due to the
-   * formula's construction, the score does not have an upper bound.
+   * The result is a number that corresponds with a U.S. grade level. The
+   * sentence, "The Australian platypus is seemingly a hybrid of a mammal and
+   * reptilian creature" is an 11.3 as it has 24 syllables and 13 words. The
+   * different weighting factors for words per sentence and syllables per word in
+   * each scoring system mean that the two schemes are not directly comparable and
+   * cannot be converted. The grade level formula emphasises sentence length over
+   * word length. By creating one-word strings with hundreds of random characters,
+   * grade levels may be attained that are hundreds of times larger than high
+   * school completion in the United States. Due to the formula's construction,
+   * the score does not have an upper bound.
    * 
-   * The lowest grade level score in theory is −3.40, but there are few real passages in which every
-   * sentence consists of a single one-syllable word. Green Eggs and Ham by Dr. Seuss comes close,
-   * averaging 5.7 words per sentence and 1.02 syllables per word, with a grade level of −1.3. (Most
-   * of the 50 used words are monosyllabic; "anywhere", which occurs eight times, is the only
+   * The lowest grade level score in theory is −3.40, but there are few real
+   * passages in which every sentence consists of a single one-syllable word.
+   * Green Eggs and Ham by Dr. Seuss comes close, averaging 5.7 words per sentence
+   * and 1.02 syllables per word, with a grade level of −1.3. (Most of the 50 used
+   * words are monosyllabic; "anywhere", which occurs eight times, is the only
    * exception.)
    * 
    * @param sentences to lex
-   * @param words parsed from sentences
+   * @param words     parsed from sentences
    * @return reading grade level
    */
   public static double calculateGradeLevel(List<Sentence> sentences, List<String> words) {
     double totalSentences = sentences.size();
     double totalWords = words.size();
     double totalSyllables = countSyllables(words);
-    
+
     return 0.39 * (totalWords / totalSentences) + 11.8 * (totalSyllables / totalWords) - 15.59;
   }
-  
+
   static double countSyllables(List<String> tokens) {
-    return tokens
-            .stream()
-            .mapToDouble(ReadabilityCalculator::countSyllables)
-            .sum();
+    return tokens.stream().mapToDouble(ReadabilityCalculator::countSyllables).sum();
   }
-  
+
   static double countSyllables(String word) {
     // null or empty strings have no syllables
     if (word == null || word.trim().isEmpty())
       return 0;
-    
+
     // words with special characters are likely abbreviated NNP or NNPS, count as 1
     if (!word.matches("[a-zA-Z]+"))
       return 1;
-    
+
     return Hyphenator.hyphen(word).syllables().size();
   }
-  
+
 }
