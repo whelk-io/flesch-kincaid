9df5d2d8b961a714a370904bb835469b4093c2f7
Zack Teater <zteater@gmail.com>
2021-02-09 18:11:13 -0500

    upgrade to junit 5, fixes #147, fixes #160, fixes #150

--- src/test/java/io/whelk/flesch/kincaid/ComprehendValidatorTest.java
+++ src/test/java/io/whelk/flesch/kincaid/ComprehendValidatorTest.java
@@ -1,60 +1,62 @@
 package io.whelk.flesch.kincaid;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import com.amazonaws.services.comprehend.model.PartOfSpeechTag;
 import com.amazonaws.services.comprehend.model.PartOfSpeechTagType;
 import com.amazonaws.services.comprehend.model.SyntaxToken;
 
-public class ComprehendValidatorTest {
+import org.junit.jupiter.api.Test;
+
+class ComprehendValidatorTest {
 
   @Test
-  public void testIsWord_withNullSyntaxToken() { 
+  void testIsWord_withNullSyntaxToken() {
     SyntaxToken syntaxToken = null;
-    
+
     var result = ComprehendValidator.isWord(syntaxToken);
     assertFalse(result);
   }
-  
+
   @Test
-  public void testIsWord_withSyntaxTokenNullPOS() { 
-    var syntaxToken = new SyntaxToken(); 
+  void testIsWord_withSyntaxTokenNullPOS() {
+    var syntaxToken = new SyntaxToken();
     syntaxToken.setPartOfSpeech(null);
-    
+
     var result = ComprehendValidator.isWord(syntaxToken);
     assertFalse(result);
   }
-  
+
   @Test
-  public void testIsWord_withSyntaxTokenNullPOSTag() { 
-    var syntaxToken = new SyntaxToken(); 
+  void testIsWord_withSyntaxTokenNullPOSTag() {
+    var syntaxToken = new SyntaxToken();
     var posTag = new PartOfSpeechTag();
     posTag.setTag(null);
     syntaxToken.setPartOfSpeech(posTag);
-    
+
     var result = ComprehendValidator.isWord(syntaxToken);
     assertFalse(result);
   }
-  
+
   @Test
-  public void testIsWord_withUnknownSyntaxToken() { 
-    var syntaxToken = new SyntaxToken(); 
+  void testIsWord_withUnknownSyntaxToken() {
+    var syntaxToken = new SyntaxToken();
     var posTag = new PartOfSpeechTag();
     posTag.setTag("foo");
     syntaxToken.setPartOfSpeech(posTag);
-    
+
     var result = ComprehendValidator.isWord(syntaxToken);
     assertFalse(result);
   }
-  
+
   @Test
-  public void testIsWord_withValidSyntaxToken() { 
-    var syntaxToken = new SyntaxToken(); 
+  void testIsWord_withValidSyntaxToken() {
+    var syntaxToken = new SyntaxToken();
     var posTag = new PartOfSpeechTag();
     posTag.setTag(PartOfSpeechTagType.NOUN.name());
     syntaxToken.setPartOfSpeech(posTag);
-    
+
     var result = ComprehendValidator.isWord(syntaxToken);
     assertTrue(result);
   }
